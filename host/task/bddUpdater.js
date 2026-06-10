e61db87239e86d9e36390533265f9e4b34b2cdf5
Blue <matias.carribero@gmail.com>
2026-06-09 21:21:33 -0300

    bddUpdater v1.0.3
    El bucle de exploracion dejaba de actuar al segundo bucle.

--- host/task/bddUpdater.js
+++ host/task/bddUpdater.js
@@ -69,6 +69,9 @@ export async function main(ns) {
                                                             \n---------------------------------------------\n`).join("");
         ns.write("servidores-listados.txt", servidoresListadosTxt, "w");
 
+        servidoresVisitados = [ { nombre: "home" } ];
+        servidoresBDD = [];
+        
         await ns.asleep(1000);
     }
 
